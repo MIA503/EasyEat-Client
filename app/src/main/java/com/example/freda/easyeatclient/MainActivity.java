fa8c8d389a0d708a3ae5ee1b9077b7bbacab5099
Xufeng Cheng”git config --global user.email s207085@studenti.polito.it” <s207085@studenti.polito.it>
2016-08-13 22:48:50 +0200

    gradlew

--- app/src/main/java/com/example/freda/easyeatclient/MainActivity.java
+++ app/src/main/java/com/example/freda/easyeatclient/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.freda.easyeatclient;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView clientList;
+    private String[] lvs = {"List Item 01", "List Item 02", "List Item 03", "List Item 04"};
+    private ArrayAdapter arrayAdapter;
+
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
+        clientList = (ListView) findViewById(R.id.clientList);
+
+        toolbar.setTitle("EasyEat");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,lvs);
+        clientList.setAdapter(arrayAdapter);
+
+
+
     }
 }
