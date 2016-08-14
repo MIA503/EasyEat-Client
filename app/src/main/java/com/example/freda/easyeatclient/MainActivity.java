7719d4706b07b46219d59773c8fe0a3324e3deaa
Xufeng Cheng”git config --global user.email s207085@studenti.polito.it” <s207085@studenti.polito.it>
2016-08-14 13:16:34 +0200

    Circle View and Client pic

--- app/src/main/java/com/example/freda/easyeatclient/MainActivity.java
+++ app/src/main/java/com/example/freda/easyeatclient/MainActivity.java
@@ -1,33 +1,52 @@
 package com.example.freda.easyeatclient;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.freda.easyeatclient.Utils.CircleImg;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int REQUESTCODE_PICK = 0;
+    private static final int REQUESTCODE_TAKE = 1;
+    private static final int REQUESTCODE_CUTTING = 2;
+    private static final String IMAGE_FILE_NAME = "clientPic.jpg";
 
     private Toolbar toolbar;
+    private Context mContext;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ListView clientList;
+    private CircleImg clientPicImg;
     private String[] lvs = {"List Item 01", "List Item 02", "List Item 03"};
     private ArrayAdapter arrayAdapter;
+    private SelectPicPopupWindow menuWindow;
 
     @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
-        clientList = (ListView) findViewById(R.id.clientList);
+        mContext = MainActivity.this;
+        initView();
 
         toolbar.setTitle("EasyEat");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
@@ -54,4 +73,114 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
+        clientList = (ListView) findViewById(R.id.clientList);
+        clientPicImg = (CircleImg) findViewById(R.id.clientPic);
+
+        clientPicImg.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clientPic:
+                menuWindow = new SelectPicPopupWindow(mContext,itemsOnClick);
+                menuWindow.showAtLocation(findViewById(R.id.mainLayout), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0,0);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private View.OnClickListener itemsOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            menuWindow.dismiss();
+            switch (v.getId()) {
+                // take photos
+                case R.id.takePhotoBtn:
+                    Intent takeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    //
+                    takeIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME)));
+                    startActivityForResult(takeIntent, REQUESTCODE_TAKE);
+                    break;
+                // choose photos
+                case R.id.choosePhotoBtn:
+                    Intent pickIntent = new Intent(Intent.ACTION_PICK, null);
+                    // image/jpeg 、 image/png...
+                    pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(pickIntent, REQUESTCODE_PICK);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case REQUESTCODE_PICK:// get pics from album
+                try {
+                    startPhotoZoom(data.getData());
+                } catch (NullPointerException e) {
+                    e.printStackTrace();//
+                }
+                break;
+            case REQUESTCODE_TAKE:// take photos
+                File temp = new File(Environment.getExternalStorageDirectory() + "/" + IMAGE_FILE_NAME);
+                startPhotoZoom(Uri.fromFile(temp));
+                break;
+            case REQUESTCODE_CUTTING:// get the pic
+                if (data != null) {
+
+                    setPicToView(data);
+
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY height width
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        intent.putExtra("outputX", 300);
+        intent.putExtra("outputY", 300);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, REQUESTCODE_CUTTING);
+    }
+
+    private void setPicToView(Intent picdata)  {
+        Bundle extras = picdata.getExtras();
+        if (extras != null) {
+            // SDCard path
+            Bitmap photo = extras.getParcelable("data");
+            Drawable drawable = new BitmapDrawable(null, photo);
+
+       //     urlpath = FileUtil.saveFile(mContext,"temphead.jpg", photo);
+            clientPicImg.setImageDrawable(drawable);
+
+         //   Storage.saveString(this,"url",urlpath);
+
+//            SaveImage(photo);
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/freda/easyeatclient/SelectPicPopupWindow.java
@@ -0,0 +1,65 @@
+package com.example.freda.easyeatclient;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+import java.io.File;
+
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+
+/**
+ * Created by freda on 8/14/16.
+ */
+public class SelectPicPopupWindow extends PopupWindow{
+   private Button takePhotoBtn,pickPhotoBtn,cancleBtn;
+    private View mMenuView;
+
+    public SelectPicPopupWindow(Context context, View.OnClickListener itemsOnClick) {
+        super(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        mMenuView = inflater.inflate(R.layout.pic_dialog,null);
+
+        takePhotoBtn = (Button) mMenuView.findViewById(R.id.takePhotoBtn);
+        pickPhotoBtn = (Button) mMenuView.findViewById(R.id.choosePhotoBtn);
+        cancleBtn = (Button) mMenuView.findViewById(R.id.cancleBtn);
+
+        cancleBtn.setOnClickListener(itemsOnClick);
+        pickPhotoBtn.setOnClickListener(itemsOnClick);
+        takePhotoBtn.setOnClickListener(itemsOnClick);
+
+        this.setContentView(mMenuView);
+        this.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        this.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        this.setFocusable(true);
+        this.setAnimationStyle(R.style.PopupAnimation);
+
+        ColorDrawable dw = new ColorDrawable(0x80000000);
+        this.setBackgroundDrawable(dw);
+
+        mMenuView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int height = mMenuView.findViewById(R.id.pop_layout).getTop();
+                int y = (int) event.getY();
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    if(y<height){
+                        dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+
+}
