86b93229a3b7e64e1f89d9653a13789e32076b70
lei_qing <leiqing010@gmail.com>
2016-08-28 17:44:12 +0200

    New changelist

---
+++ app/src/main/java/com/example/freda/easyeatclient/entity/test.java
@@ -0,0 +1,7 @@
+package com.example.freda.easyeatclient.entity;
+
+/**
+ * Created by lq on 11/05/16.
+ */
+public class test {
+}
---
+++ app/src/test/java/com/example/freda/easyeatclient/adapter/ShopOrderDishesListViewAdapterTest.java
@@ -0,0 +1,10 @@
+package com.example.freda.easyeatclient.adapter;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lq on 28/08/16.
+ */
+public class ShopOrderDishesListViewAdapterTest {
+
+}
